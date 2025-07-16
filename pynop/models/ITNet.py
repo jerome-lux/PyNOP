64a5add695cbe1cba88586223f4edd70408a8b7b
jlux <jerome.lux78@gmail.com>
2025-07-16 13:31:17 +0200

    refactor: clean imports

--- pynop/models/ITNet.py
+++ pynop/models/ITNet.py
@@ -4,7 +4,7 @@ import torch.nn.functional as F
 from typing import Union, Sequence, Callable
 import collections.abc as abc
 from pynop.core.norm import LayerNorm2d
-from pynop.core.blocks import SNLITBlock, LITBlock, ANLITBlock
+from pynop.core.blocks import LITBlock
 
 
 class CITNet(nn.Module):
