d8a0ba5034ac0ca501f78ce3ea6281e80c4a21b9
jlux <jerome.lux78@gmail.com>
2026-01-10 15:04:53 +0100

    feat: enhance Model class with autoregressive step tracking and average loss logging

--- pynop/training/lightning_model.py
+++ pynop/training/lightning_model.py
@@ -1,3 +1,4 @@
+import math
 import pytorch_lightning as pl
 import torch
 import torch.optim as optim
@@ -6,6 +7,7 @@ from pathlib import Path
 import os
 import json
 from typing import Any
+from torchmetrics import MeanMetric
 from attr import dataclass
 
 default_scheduler_config = [
@@ -27,6 +29,7 @@ default_opt_config = {"optimizer": "AdamW", "lr": 1e-3, "betas": (0.9, 0.999), "
 class TrainingSchedule:
     start_autoregressive: Any = None  # start o fthe autoregressive training
     final_autoregressive: Any = None  # epoch at which the number of autoregressive steps is maximum
+    min_autoregressive_steps: int = 0  # minimum number of autoregressive steps (after start_autoregressive epochs)
     max_autoregressive_steps: int = 0  # maximum number of autoregressive steps
     detach_grad_steps: int = 4  # number of steps before detaching the gradient in autoregressive mode
     loss_fn: Any = torch.nn.MSELoss()
@@ -48,6 +51,7 @@ class Model(pl.LightningModule):
         self.scheduler_config = scheduler_config
         self.loss_fn = self.train_config.loss_fn
         self.detach_every_k = self.train_config.detach_grad_steps
+        self.train_loss_avg = MeanMetric()
 
     def forward(self, x, training=True):
         return self.model(x, training=training)
@@ -84,6 +88,15 @@ class Model(pl.LightningModule):
         else:
             return self.optimizer
 
+    def on_train_epoch_start(self):
+        self.train_loss_avg.reset()
+
+    # def on_before_optimizer_step(self, optimizer):
+    #     # Calcule la norme L2 globale de tous les poids du modèle
+    #     norms = [p.norm(2) for p in self.model.parameters() if p.grad is not None]
+    #     total_norm = torch.norm(torch.stack(norms))
+    #     self.log("weight_norm", total_norm, prog_bar=True, on_step=True)
+
     def training_step(self, batch, batch_idx):
 
         inputs, time_idx = batch
@@ -91,20 +104,20 @@ class Model(pl.LightningModule):
 
         epoch = self.current_epoch
         max_AR_steps = min(T_unroll - 1, self.train_config.max_autoregressive_steps)
+        min_AR_steps = max(self.train_config.min_autoregressive_steps, 1)
 
         if self.train_config.start_autoregressive is not None and self.train_config.final_autoregressive is not None:
-            if epoch < self.train_config.start_autoregressive:
-                progress = 0.0
-            elif epoch >= self.train_config.final_autoregressive:
-                progress = 1.0
+            nint = max_AR_steps - min_AR_steps + 1
+            delta = (self.train_config.final_autoregressive - self.train_config.start_autoregressive) // nint
+            if delta > 0:
+                AR_steps = min(max(min_AR_steps + (epoch // delta), 0), max_AR_steps)
             else:
-                progress = (epoch - self.train_config.start_autoregressive) / (
-                    self.train_config.final_autoregressive - self.train_config.start_autoregressive
-                )
+                AR_steps = max_AR_steps
 
-            threshold = (T_unroll - 1) - int(progress * max_AR_steps)
         else:
-            threshold = T_unroll - 1
+            AR_steps = 0
+
+        threshold = (T_unroll - 1) - AR_steps
 
         loss = 0.0
 
@@ -135,8 +148,11 @@ class Model(pl.LightningModule):
                 preds = preds.detach()
 
         loss = loss / (T_unroll - 1)
+        self.train_loss_avg.update(loss)
 
-        self.log("loss", torch.sqrt(loss), on_step=True, on_epoch=True, prog_bar=True, logger=True)
+        self.log("loss", loss, on_step=True, on_epoch=True, prog_bar=True, logger=True)
+        self.log("avg_loss", self.train_loss_avg.compute(), prog_bar=True)
+        self.log("AR_steps", int(AR_steps), on_step=True, prog_bar=True, on_epoch=True, logger=True)
 
         return loss
 
@@ -146,20 +162,20 @@ class Model(pl.LightningModule):
 
         epoch = self.current_epoch
         max_AR_steps = min(T_unroll - 1, self.train_config.max_autoregressive_steps)
+        min_AR_steps = max(self.train_config.min_autoregressive_steps, 1)
 
         if self.train_config.start_autoregressive is not None and self.train_config.final_autoregressive is not None:
-            if epoch < self.train_config.start_autoregressive:
-                progress = 0.0
-            elif epoch >= self.train_config.final_autoregressive:
-                progress = 1.0
+            nint = max_AR_steps - min_AR_steps + 1
+            delta = (self.train_config.final_autoregressive - self.train_config.start_autoregressive) // nint
+            if delta > 0:
+                AR_steps = min(max(min_AR_steps + (epoch // delta), 0), max_AR_steps)
             else:
-                progress = (epoch - self.train_config.start_autoregressive) / (
-                    self.train_config.final_autoregressive - self.train_config.start_autoregressive
-                )
+                AR_steps = max_AR_steps
 
-            threshold = (T_unroll - 1) - int(progress * max_AR_steps)
         else:
-            threshold = T_unroll - 1
+            AR_steps = 0
+
+        threshold = (T_unroll - 1) - AR_steps
 
         loss = 0.0
 
@@ -192,6 +208,6 @@ class Model(pl.LightningModule):
 
         loss = loss / (T_unroll - 1)
 
-        self.log("val_loss", torch.sqrt(loss), on_step=True, on_epoch=True, prog_bar=True, logger=True)
+        self.log("val_loss", loss, on_step=True, on_epoch=True, prog_bar=True, logger=True)
 
         return loss
