72fcaa419ef98cd079d10ae791eb6e17791bc8a8
jlux <jerome.lux78@gmail.com>
2026-05-07 17:19:29 +0200

    add GRN (Global Response Normalization) layer with learnable parameters

--- pynop/core/norm.py
+++ pynop/core/norm.py
@@ -112,6 +112,20 @@ class RMSNorm2d(nn.RMSNorm):
         return x
 
 
+class GRN(nn.Module):
+    """GRN (Global Response Normalization) layer"""
+
+    def __init__(self, dim):
+        super().__init__()
+        self.gamma = nn.Parameter(torch.zeros(1, 1, 1, dim))
+        self.beta = nn.Parameter(torch.zeros(1, 1, 1, dim))
+
+    def forward(self, x):
+        Gx = torch.norm(x, p=2, dim=(1, 2), keepdim=True)
+        Nx = Gx / (Gx.mean(dim=-1, keepdim=True) + 1e-6)
+        return self.gamma * (x * Nx) + self.beta + x
+
+
 NORM_DICT = {"ln": LayerNorm2d, "gn": nn.GroupNorm, "bn": nn.BatchNorm2d, "rmsn": RMSNorm2d}
 
 
